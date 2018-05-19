af1936e83c545a794df61666c87b7d02891b6647
BacNguyen <nguyenvanbac12a2@gmail.com>
2018-05-19 09:25:42 +0700

    Reconnect to firebase

---
+++ app/src/main/java/bbteam/com/application/fragment/AddFamilyFragment.java
@@ -0,0 +1,44 @@
+package bbteam.com.application.fragment;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import bbteam.com.applocation.R;
+
+public class AddFamilyFragment extends DialogFragment {
+
+
+
+    public AddFamilyFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_add_family, null);
+        final EditText userTextView = view.findViewById(R.id.txt_family_name);
+        builder.setView(view)
+                .setMessage("Add Family")
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
